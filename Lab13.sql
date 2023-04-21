2d77fa0b787204f570f1fcf14119f0301c1853e6
nguyencongtruong0950080039 <119740674+nguyencongtruong0950080039@users.noreply.github.com>
2023-04-21 15:03:58 +0700

    Add files via upload

---
+++ Lab13.sql
@@ -0,0 +1,233 @@
+﻿
+---------Cau1-------
+CREATE DATABASE QLBH;
+
+USE QLBH;
+
+CREATE TABLE MATHANG (
+  mahang INT PRIMARY KEY,
+  tenhang VARCHAR(50) NOT NULL,
+  soluong INT NOT NULL
+);
+
+CREATE TABLE NHATKYBANHANG (
+  stt INT PRIMARY KEY,
+  ngay DATE NOT NULL,
+  nguoimua VARCHAR(50) NOT NULL,
+  mahang INT NOT NULL,
+  soluong INT NOT NULL,
+  giaban FLOAT NOT NULL,
+  FOREIGN KEY (mahang) REFERENCES MATHANG(mahang)
+);
+
+--Cau2--
+INSERT INTO MATHANG (mahang, tenhang, soluong)
+VALUES 
+  (1, 'Keo', 100),
+  (2, 'Banh', 200),
+  (3, 'Thuoc', 100);
+ 
+INSERT INTO NHATKYBANHANG (stt, ngay, nguoimua, mahang, soluong, giaban)
+VALUES
+  (1, '1999-02-09', 'ab', 2, 230, 500000);
+  
+
+--------a--------
+go
+CREATE TRIGGER trg_nhatkybanhang_insert
+ON NHATKYBANHANG
+AFTER INSERT
+AS
+BEGIN
+	UPDATE MATHANG
+	SET soluong = MATHANG.soluong - inserted.soluong
+	FROM MATHANG
+	INNER JOIN inserted ON MATHANG.mahang = inserted.mahang
+	END;
+
+-------b---------
+	go
+	CREATE TRIGGER trg_nhatkybanhang_update
+	ON NHATKYBANHANG
+	AFTER UPDATE
+	AS
+	BEGIN
+		IF UPDATE(soluong)
+		BEGIN
+		UPDATE MATHANG
+		SET soluong = MATHANG.soluong + deleted.soluong - inserted.soluong
+		FROM MATHANG
+		INNER JOIN deleted ON MATHANG.mahang = deleted.mahang
+		INNER JOIN inserted ON MATHANG.mahang = inserted.mahang
+	END
+END;
+
+-------c------
+go
+CREATE TRIGGER trg_nhatkybanhang_insert_1
+ON NHATKYBANHANG
+FOR INSERT
+AS
+BEGIN
+	DECLARE @mahang INT, @soluong INT, @soluong_hien_co INT
+
+	SELECT @mahang = mahang, @soluong = soluong
+	FROM inserted
+
+	SELECT @soluong_hien_co = soluong
+	FROM MATHANG
+	WHERE mahang = @mahang
+
+	IF @soluong <= @soluong_hien_co
+	BEGIN
+		UPDATE MATHANG
+		SET soluong = soluong - @soluong
+		WHERE mahang = @mahang
+		END
+		ELSE
+		BEGIN
+		RAISERROR('Số lượng hàng bán ra phải nhỏ hơn hoặc bằng số lượng hàng hiện có!', 16, 1)
+		ROLLBACK TRANSACTION
+	END
+END;
+
+--------d-------
+go
+CREATE TRIGGER trg_nhatkybanhang_update_1
+ON NHATKYBANHANG
+FOR UPDATE
+AS
+BEGIN
+IF (SELECT COUNT(*) FROM inserted) > 1
+BEGIN
+	RAISERROR('Chỉ được cập nhật 1 bản ghi tại một thời điểm!', 16, 1)
+	ROLLBACK TRANSACTION
+	END
+	ELSE
+	BEGIN
+		DECLARE @mahang INT, @soluong INT, @soluong_hien_co INT
+		SELECT @mahang = mahang, @soluong = soluong
+		FROM inserted
+
+		SELECT @soluong_hien_co = soluong
+		FROM MATHANG
+		WHERE mahang = @mahang
+
+		UPDATE MATHANG
+		SET soluong = soluong + (SELECT soluong FROM deleted) - @soluong
+		WHERE mahang = @mahang
+	END
+END;
+
+--------e-------
+go
+CREATE TRIGGER trg_nhatkybanhang_delete
+ON NHATKYBANHANG
+FOR DELETE
+AS
+BEGIN
+	IF (SELECT COUNT(*) FROM deleted) > 1
+	BEGIN
+		RAISERROR('Chỉ được xóa 1 bản ghi tại một thời điểm!', 16, 1)
+		ROLLBACK TRANSACTION
+		END
+		ELSE
+		BEGIN
+		DECLARE @mahang INT, @soluong INT
+		SELECT @mahang = mahang, @soluong = soluong
+		FROM deleted
+		UPDATE MATHANG
+		SET soluong = soluong + @soluong
+		WHERE mahang = @mahang
+	END
+END;
+
+----------f--------
+go
+CREATE TRIGGER trg_nhatkybanhang_update_2
+ON NHATKYBANHANG
+FOR UPDATE
+AS
+BEGIN
+	DECLARE @mahang INT, @soluong INT, @soluong_hien_co INT
+
+	SELECT @mahang = mahang, @soluong = soluong
+	FROM inserted
+
+	SELECT @soluong_hien_co = soluong
+	FROM MATHANG
+	WHERE mahang = @mahang
+
+	IF @soluong > @soluong_hien_co
+	BEGIN
+		RAISERROR('Số lượng cập nhật không được vượt quá số lượng hiện có!', 16, 1)
+		ROLLBACK TRANSACTION
+	END
+	ELSE IF @soluong = @soluong_hien_co
+	BEGIN
+		RAISERROR('Không cần cập nhật số lượng!', 16, 1)
+		ROLLBACK TRANSACTION
+	END
+	ELSE
+	BEGIN
+		UPDATE MATHANG
+		SET soluong = soluong + (SELECT soluong FROM deleted) - @soluong
+		WHERE mahang = @mahang
+	END
+END;
+
+--------g----------
+go
+CREATE PROCEDURE sp_xoa_mathang
+@mahang INT
+AS
+BEGIN
+IF NOT EXISTS (SELECT * FROM MATHANG WHERE mahang = @mahang)
+BEGIN
+PRINT 'Mã hàng không tồn tại!'
+RETURN
+END
+
+BEGIN TRANSACTION
+
+DELETE FROM NHATKYBANHANG WHERE mahang = @mahang
+DELETE FROM MATHANG WHERE mahang = @mahang
+
+COMMIT TRANSACTION
+
+PRINT 'Xóa mặt hàng thành công!'
+END
+
+
+-----i-----
+-- Test cho câu 2
+SELECT * FROM MATHANG
+SELECT * FROM NHATKYBANHANG
+-- Test cho câu 3a
+INSERT INTO NHATKYBANHANG (stt, ngay, nguoimua, mahang, soluong, giaban)
+VALUES
+(4, '2021-03-21', 'Nguyễn A', 2, 3, 150000)
+SELECT * FROM NHATKYBANHANG
+-- Test cho câu 3b
+UPDATE NHATKYBANHANG SET soluong = 2 WHERE stt = 2
+SELECT * FROM MATHANG
+-- Test cho câu 3c
+INSERT INTO NHATKYBANHANG (stt, ngay, nguoimua, mahang, soluong, giaban)
+VALUES
+(5, '2021-05-22', 'Trương C', 2, 10, 120000)
+SELECT * FROM MATHANG
+-- Test cho câu 3d
+UPDATE NHATKYBANHANG SET soluong = 10 WHERE stt = 2 
+SELECT * FROM MATHANG
+-- Test cho câu 3e
+DELETE FROM NHATKYBANHANG WHERE stt = 4
+SELECT * FROM MATHANG
+-- Test cho câu 3f
+UPDATE NHATKYBANHANG SET soluong = 7 WHERE stt = 5
+SELECT * FROM MATHANG
+-- Test cho câu 3g
+EXEC sp_xoa_mathang 3
+SELECT * FROM MATHANG
+SELECT * FROM NHATKYBANHANG
+
+
